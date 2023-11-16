0576e797b5df34c1677f50661f500744383354ce
Umar268 <95554503+Umar268@users.noreply.github.com>
2023-11-16 11:39:08 +0100

    first commit

---
+++ Java Script/Js/mega.js
@@ -0,0 +1,40 @@
+var x = 7;
+var y = 9;
+var z = x * y;
+console.log(z);
+
+
+const car= {
+    name:"Bnently",
+    brand:"Audi",
+    horsepower:"R8",
+    price:45098787,
+    transmision:"Manual",
+    color:"White"
+}
+console.log(car);
+
+
+const cars = ["Mazda", "Buggati", "Ferrari", "Tesla", "Chevrolet"] ;
+console.log(cars);
+
+ const states = ["kano",    {
+    capital1:"Kano",
+    capital2:"Kadauna",
+    capiatal3:"Dutse",
+    capital4:"Ikeja",
+}
+]
+
+
+console.log(states[1].capital2)
+
+const person = {
+    firstname: "Ibrahim",
+    lastname: "Tasiu",
+    age:50,
+    // eyecolor:"Blue",
+    // skincolor :"Mungloid",
+    // hobby: "Programming"
+};
+console.log(person);
---
+++ Java Script/script.js
@@ -0,0 +1,216 @@
+// Changing html content
+// document.getElementById("demo").innerHTML="Hello Javascript";
+
+//changing or addiing style to the content
+// document.getElementById("demo").style.fontSize = "25px"
+// document.getElementById("demo").style.color ="white"
+// document.getElementById("demo").style.background="black"
+
+// Change content
+const Btn = document.getElementById("ChangeBtn");
+// functions
+function CahangeContent() {
+    document.getElementById("demo").innerHTML="Hello Javascript";
+
+}
+Btn.onclick = () => CahangeContent();
+
+// Changing color
+const btn = document.getElementById("Changebtn");
+
+function ChangeColor() {
+    document.getElementById("demo").style.color = "green";
+}
+btn.onclick = () => ChangeColor();
+
+// Hide content
+const bTn = document.getElementById("Hide");
+
+function Hide() {
+    document.getElementById("demo").style.display ="none";
+}
+bTn.onclick = () => Hide();
+
+// Show content 
+const BTN = document.getElementById("Showbtn");
+
+function Showbtn() {
+    document.getElementById("demo").style.display ="block";
+}
+BTN.onclick = () => Showbtn();
+
+
+window.alert("hello world");
+console.log("hello world")
+
+
+// ? statements
+const boyName = "jesse"
+
+// var , let , const 
+var girlName = "jan";
+let workName = "BIAe";
+const cityName = "Jos";
+var x , y , z , gg;
+x = 5;
+y = 6;
+z = x + y;
+gg = 72
+console.log( x + y + z) ; 
+var sk = gg
+console.log(sk)
+ var schoolName = "Unijos"
+console.log(schoolName)
+const string = "52585 + 1000 / 520*9625";
+console.log(string)
+
+const name1 = "10";
+const name2 = "50"
+console.log(name1 +  name2) 
+
+var x , y;
+x = 5 + 9;
+y = x * 10;
+console.log(y);
+
+var lastname, lastName;
+lastname = "Isa";
+lastName = "Bashir"
+console.log(lastname,);
+console.log(lastName);
+
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+
+var carName 
+carName = "Lexus"
+console.log(carName);
+
+var carName = "Audi R8"
+console.log(carName);
+
+let name = 'Umar'
+console.log(name);
+
+let ftName ='Umar';
+let LtName = 'Abdulaziz'
+console.log(ftName, LtName)
+
+ interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let isApproved = true ;
+
+let person = {
+    fname: 'Umar',
+    // lname: 'Abdulaziz',
+    age: 20,
+};
+
+person.fname='Musa Isa ';
+console.log(person.fname);
+
+// concatenating two names
+var txt1 = "Hamza";
+var txt2 = "Usman";
+var txt3 = txt1 + " " + txt2;
+console.log(txt3);
+
+tx1 = "What a ";
+tx1 += "wonderful day."
+console.log(tx1);
+
+// Adding number with string 
+var x= 6 + 9;
+var y = "6" + 8;
+var z= "Hello" + 8
+console.log(x);
+console.log(y);
+console.log(z);
+ 
+// Arithmetic operetion
+var a = 100;
+var b = 50;
+var c = a + b;
+console.log(c)
+
+var x = (100 + 9) * a;
+console.log(x)
+
+var h =150 * 10;
+console.log(h);
+
+var e = 56;
+var t = 45;
+var g = e - t;
+console.log(g);
+
+var n = 78 ;
+var u = 89;
+var f = n / u;
+console.log(f);
+
+var j = 56;
+var k = 3;
+var l = j % k;
+console.log(l);
+
+var x = 69;
+x++;
+z = x ;
+console.log(z);
+
+var y = 90;
+y--;
+z = y;
+console.log(z)
+
+var x = 16 + "volvlo"
+console.log(x);
+
+var x ;
+x = 5;
+x = "Ibrahim";
+console.log(x);
+
+var x = 456e6;
+var y=  46e9;
+console.log(x);
+console.log(y);
+
+var x = 5;
+var y = 9;
+var z = 8;
+(x == y)
+(y == z) 
+(z == x)
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// var x = myfunction(9,8);
+
+// myfunction(a, b) {
+//     return a * b; 
+//     }
+
+const car = {
+    name:"Bnently",
+    brand:"Audi",
+    horsepower:"R8",
+    price:"$450,987",
+    transmision:"Manual",
+    color:"White"
+}
+console.log.(car);
+
+
+
+
+
+
+
